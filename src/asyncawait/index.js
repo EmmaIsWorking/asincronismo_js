5d98510af25de73fda1c0012fd0c680274238cbf
EmmaIsWorking <dimple.pleod@gmail.com>
2020-04-27 16:29:12 -0500

    update

---
+++ src/asyncawait/index.js
@@ -0,0 +1,29 @@
+const doSomethingAsync = ()=>{
+  return new Promise((resolve, reject)=>{
+    (true)
+      ? setTimeout(()=>resolve('resolve'),4000)
+      : reject(new Error('error'))
+  })
+}
+
+const doSomething = async ()=>{
+  const doSomething = await doSomethingAsync()
+  console.log(doSomething)
+}
+
+console.log('Before1');
+doSomething()
+console.log('After2');
+
+const doSomething2 = async ()=>{
+  try {
+    const Something = await doSomethingAsync()
+    console.log(Something)
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+console.log('Before1');
+doSomething2()
+console.log('After2');
